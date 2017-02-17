5d13ac02afc366699c9dc8b50405b10b4727f22b
Henrique Souza <henriquesouza182@gmail.com>
2017-02-16 23:19:58 -0200

    Nothing Change

---
+++ NiceTry.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,15 @@
+﻿using NiceTry.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceTry.LojaVirtual.Dominio.Repositorio
+{
+    class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ NiceTry.LojaVirtual.Dominio/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using NiceTry.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceTry.LojaVirtual.Dominio.Repositorio
+{
+    class ProdutoRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
